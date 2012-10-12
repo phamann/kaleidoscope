8374a56d522be1e0b47e05ecb51d5889883d1a1f
Patrick Hamann <patrick.hamann@guardian.co.uk>
2012-10-12 15:01:24 +0100

    Fixed major fuck up

--- kaleidoscope.user.js
+++ kaleidoscope.user.js
@@ -18,7 +18,6 @@ var mobileSvg  = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5v
 var loadingGif = 'data:image/gif;base64,R0lGODlhIAAgAPMAAN3d3QAAAKysrHJycp6enoWFhS4uLkpKSru7u8bGxqOjoxoaGgMDAwAAAAAAAAAAACH/C05FVFNDQVBFMi4w AwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAIAAgAAAE5xDISWlhperN52JLhSSdRgwVo1IC QZRUsiwHpTJT4iowNS8vyW2icCF6k8HMMBkCEDskxTBDAZwuAkkqIfxIQyhBQBFvAQSDITM5VDW6XNE4KagNh6Bgwe60smQUB3d4 Rz1ZBApnFASDd0hihh12BkE9kjAJVlycXIg7CQIFA6SlnJ87paqbSKiKoqusnbMdmDC2tXQlkUhziYtyWTxIfy6BE8WJt5YJvpJi vxNaGmLHT0VnOgSYf0dZXS7APdpB309RnHOG5gDqXGLDaC457D1zZ/V/nmOM82XiHRLYKhKP1oZmADdEAAAh+QQJCgAAACwAAAAA IAAgAAAE6hDISWlZpOrNp1lGNRSdRpDUolIGw5RUYhhHukqFu8DsrEyqnWThGvAmhVlteBvojpTDDBUEIFwMFBRAmBkSgOrBFZog CASwBDEY/CZSg7GSE0gSCjQBMVG023xWBhklAnoEdhQEfyNqMIcKjhRsjEdnezB+A4k8gTwJhFuiW4dokXiloUepBAp5qaKpp6+H o7aWW54wl7obvEe0kRuoplCGepwSx2jJvqHEmGt6whJpGpfJCHmOoNHKaHx61WiSR92E4lbFoq+B6QDtuetcaBPnW6+O7wDHpIiK 9SaVK5GgV543tzjgGcghAgAh+QQJCgAAACwAAAAAIAAgAAAE7hDISSkxpOrN5zFHNWRdhSiVoVLHspRUMoyUakyEe8PTPCATW9A1 4E0UvuAKMNAZKYUZCiBMuBakSQKG8G2FzUWox2AUtAQFcBKlVQoLgQReZhQlCIJesQXI5B0CBnUMOxMCenoCfTCEWBsJColTMANl dx15BGs8B5wlCZ9Po6OJkwmRpnqkqnuSrayqfKmqpLajoiW5HJq7FL1Gr2mMMcKUMIiJgIemy7xZtJsTmsM4xHiKv5KMCXqfyUCJ EonXPN2rAOIAmsfB3uPoAK++G+w48edZPK+M6hLJpQg484enXIdQFSS1u6UhksENEQAAIfkECQoAAAAsAAAAACAAIAAABOcQyEmp GKLqzWcZRVUQnZYg1aBSh2GUVEIQ2aQOE+G+cD4ntpWkZQj1JIiZIogDFFyHI0UxQwFugMSOFIPJftfVAEoZLBbcLEFhlQiqGp1V d140AUklUN3eCA51C1EWMzMCezCBBmkxVIVHBWd3HHl9JQOIJSdSnJ0TDKChCwUJjoWMPaGqDKannasMo6WnM562R5YluZRwur0w pgqZE7NKUm+FNRPIhjBJxKZteWuIBMN4zRMIVIhffcgojwCF117i4nlLnY5ztRLsnOk+aV+oJY7V7m76PdkS4trKcdg0Zc0tTcKk RAAAIfkECQoAAAAsAAAAACAAIAAABO4QyEkpKqjqzScpRaVkXZWQEximw1BSCUEIlDohrft6cpKCk5xid5MNJTaAIkekKGQkWyKH kvhKsR7ARmitkAYDYRIbUQRQjWBwJRzChi9CRlBcY1UN4g0/VNB0AlcvcAYHRyZPdEQFYV8ccwR5HWxEJ02YmRMLnJ1xCYp0Y5id pQuhopmmC2KgojKasUQDk5BNAwwMOh2RtRq5uQuPZKGIJQIGwAwGf6I0JXMpC8C7kXWDBINFMxS4DKMAWVWAGYsAdNqW5uaRxkSK JOZKaU3tPOBZ4DuK2LATgJhkPJMgTwKCdFjyPHEnKxFCDhEAACH5BAkKAAAALAAAAAAgACAAAATzEMhJaVKp6s2nIkolIJ2WkBSh pkVRWqqQrhLSEu9MZJKK9y1ZrqYK9WiClmvoUaF8gIQSNeF1Er4MNFn4SRSDARWroAIETg1iVwuHjYB1kYc1mwruwXKC9gmsJXli Gxc+XiUCby9ydh1sOSdMkpMTBpaXBzsfhoc5l58Gm5yToAaZhaOUqjkDgCWNHAULCwOLaTmzswadEqggQwgHuQsHIoZCHQMMQgQG ubVEcxOPFAcMDAYUA85eWARmfSRQCdcMe0zeP1AAygwLlJtPNAAL19DARdPzBOWSm1brJBi45soRAWQAAkrQIykShQ9wVhHCwCQC ACH5BAkKAAAALAAAAAAgACAAAATrEMhJaVKp6s2nIkqFZF2VIBWhUsJaTokqUCoBq+E71SRQeyqUToLA7VxF0JDyIQh/MVVPMt1E CZlfcjZJ9mIKoaTl1MRIl5o4CUKXOwmyrCInCKqcWtvadL2SYhyASyNDJ0uIiRMDjI0Fd30/iI2UA5GSS5UDj2l6NoqgOgN4gksE BgYFf0FDqKgHnyZ9OX8HrgYHdHpcHQULXAS2qKpENRg7eAMLC7kTBaixUYFkKAzWAAnLC7FLVxLWDBLKCwaKTULgEwbLA4hJtOkS BNqITT3xEgfLpBtzE/jiuL04RGEBgwWhShRgQExHBAAh+QQJCgAAACwAAAAAIAAgAAAE7xDISWlSqerNpyJKhWRdlSAVoVLCWk6J KlAqAavhO9UkUHsqlE6CwO1cRdCQ8iEIfzFVTzLdRAmZX3I2SfZiCqGk5dTESJeaOAlClzsJsqwiJwiqnFrb2nS9kmIcgEsjQydL iIlHehhpejaIjzh9eomSjZR+ipslWIRLAgMDOR2DOqKogTB9pCUJBagDBXR6XB0EBkIIsaRsGGMMAxoDBgYHTKJiUYEGDAzHC9EA CcUGkIgFzgwZ0QsSBcXHiQvOwgDdEwfFs0sDzt4S6BK4xYjkDOzn0unFeBzOBijIm1Dgmg5YFQwsCMjp1oJ8LyIAACH5BAkKAAAA LAAAAAAgACAAAATwEMhJaVKp6s2nIkqFZF2VIBWhUsJaTokqUCoBq+E71SRQeyqUToLA7VxF0JDyIQh/MVVPMt1ECZlfcjZJ9mIK oaTl1MRIl5o4CUKXOwmyrCInCKqcWtvadL2SYhyASyNDJ0uIiUd6GGl6NoiPOH16iZKNlH6KmyWFOggHhEEvAwwMA0N9GBsEC6am hnVcEwavDAazGwIDaH1ipaYLBUTCGgQDA8NdHz0FpqgTBwsLqAbWAAnIA4FWKdMLGdYGEgraigbT0OITBcg5QwPT4xLrROZL6AuQ APUS7bxLpoWidY0JtxLHKhwwMJBTHgPKdEQAACH5BAkKAAAALAAAAAAgACAAAATrEMhJaVKp6s2nIkqFZF2VIBWhUsJaTokqUCoB q+E71SRQeyqUToLA7VxF0JDyIQh/MVVPMt1ECZlfcjZJ9mIKoaTl1MRIl5o4CUKXOwmyrCInCKqcWtvadL2SYhyASyNDJ0uIiUd6 GAULDJCRiXo1CpGXDJOUjY+Yip9DhToJA4RBLwMLCwVDfRgbBAaqqoZ1XBMHswsHtxtFaH1iqaoGNgAIxRpbFAgfPQSqpbgGBqUD 1wBXeCYp1AYZ19JJOYgH1KwA4UBvQwXUBxPqVD9L3sbp2BNk2xvvFPJd+MFCN6HAAIKgNggY0KtEBAAh+QQJCgAAACwAAAAAIAAg AAAE6BDISWlSqerNpyJKhWRdlSAVoVLCWk6JKlAqAavhO9UkUHsqlE6CwO1cRdCQ8iEIfzFVTzLdRAmZX3I2SfYIDMaAFdTESJea EDAIMxYFqrOUaNW4E4ObYcCXaiBVEgULe0NJaxxtYksjh2NLkZISgDgJhHthkpU4mW6blRiYmZOlh4JWkDqILwUGBnE6TYEbCgev r0N1gH4At7gHiRpFaLNrrq8HNgAJA70AWxQIH1+vsYMDAzZQPC9VCNkDWUhGkuE5PxJNwiUK4UfLzOlD4WvzAHaoG9nxPi5d+jYU qfAhhykOFwJWiAAAIfkECQoAAAAsAAAAACAAIAAABPAQyElpUqnqzaciSoVkXVUMFaFSwlpOCcMYlErAavhOMnNLNo8KsZsMZItJ EIDIFSkLGQoQTNhIsFehRww2CQLKF0tYGKYSg+ygsZIuNqJksKgbfgIGepNo2cIUB3V1B3IvNiBYNQaDSTtfhhx0CwVPI0UJe0+b m4g5VgcGoqOcnjmjqDSdnhgEoamcsZuXO1aWQy8KAwOAuTYYGwi7w5h+Kr0SJ8MFihpNbx+4Erq7BYBuzsdiH1jCAzoSfl0rVirN bRXlBBlLX+BP0XJLAPGzTkAuAOqb0WT5AH7OcdCm5B8TgRwSRKIHQtaLCwg1RAAAOwAAAAAAAAAAAA==';
 var closeButtonSvg = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4PSIwcHgiIHk9IjBweCIgd2lkdGg9IjEwMHB4IiBoZWlnaHQ9IjEwMHB4IiB2aWV3Qm94PSIwIDAgMTAwIDEwMCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgMTAwIDEwMDsiIHhtbDpzcGFjZT0icHJlc2VydmUiPgo8cG9seWdvbiBzdHlsZT0iZmlsbDojMDEwMTAxOyIgcG9pbnRzPSIxMDAsNzkuOTk0IDY5Ljk3OCw1MCAxMDAsMjAuMDA2IDc5Ljk5NCwwIDUwLDMwLjAwOCAxOS45OTIsMCAwLDIwLjAwNiAyOS45OTQsNTAgMCw3OS45OTQgICAxOS45OTIsMTAwIDUwLDY5Ljk5MiA3OS45OTQsMTAwICIvPgo8L3N2Zz4=';
 
-window.dragging = false;
 
 /**
  * Big inline css string
@@ -53,7 +52,7 @@ css_prefix + "phone { " +
 "}" +
 css_prefix + "container { " +
     "position: fixed;" +
-    "top: 50px;" +
+    "top: 80px;" +
     "right: 20px;" +
     "z-index: 10000;" +
     "width: 369px;" +
@@ -89,9 +88,19 @@ css_prefix + "btn-mdot {" +
 "}" +
 css_prefix + "btn-aggregator {" +
     "right: 34px;" +
+"}" +
+css_prefix + "aggregator-inner {" +
+    "padding: 10px;" +
+    "overflow: auto;" +
+    "height: 450px;" +
+"}" +
+css_prefix + "aggregator-inner h4 { " +
+    "font-family: arial;" +
+    "margin-bottom: 10px;" +
+"}" +
+css_prefix + "aggregator-inner li { " +
+    "margin: 5px 0 !important;" +
 "}";
-
-
 
 /**
  * Global object - nothing should be outside this
@@ -138,6 +147,7 @@ var Kaleidoscope = {
         cta.onclick = function() {
 
             if (!that.config.isOpen) { // first click
+                that.config.isOpen = true;
                 svg.src = loadingGif;
                 that.loadFrame();
             } else { // toggling
@@ -216,6 +226,10 @@ var Kaleidoscope = {
         ];
 
         this.config.frame_src = "http://beta.guardian.co.uk"+this.config.path+"?gu.prefs.font-family=1";
+
+        this.config.aggregator_base_url = 'http://aggregator.guardian.co.uk/v2/';
+        this.config.aggregator_networkfront_url = this.config.aggregator_base_url + 'homepage.json';
+        this.config.aggregator_section_base_url = this.config.aggregator_base_url + 'section/';
     },
 
     /**
@@ -226,15 +240,9 @@ var Kaleidoscope = {
         var that = this;
         var container = document.createElement('div');
         container.className = html_prefix + "container";
-
-        // if(localStorage.getItem(html_prefix + 'xpos')) {
-        //    container.style.top = localStorage.getItem(html_prefix + 'ypos');
-        //    container.style.left = localStorage.getItem(html_prefix + 'xpos');
-        // }
-
         document.body.appendChild(container);
 
-        this.initDragging(container);
+        that.initDragging(container);
 
         for (var i in this.config.images) {
             var img = this.config.images[i];
@@ -271,8 +279,8 @@ var Kaleidoscope = {
                 el = 'iframe',
                 state = 'off';
 
-            if(this.config.types[t] === 'responsive') state = 'on';
-            if(this.config.types[t] === 'aggregator') el = 'div';
+            if(this.config.types[t] === 'responsive') { state = 'on'; }
+            if(this.config.types[t] === 'aggregator') { el = 'div'; }
 
             this.createContainer(type, el, container, state);
         }
@@ -294,6 +302,9 @@ var Kaleidoscope = {
 
         if(element === 'iframe') {
             el.setAttribute('frameborder', '0');
+        }
+
+        if (type === "responsive") {
             el.onload = this.frameLoaded;
         }
 
@@ -320,7 +331,7 @@ var Kaleidoscope = {
                 el = document.getElementsByClassName(html_prefix + 'mdot')[0];
                 this.loadMdot(el);
                 break;
-            case 'aggregator'  :
+            case 'aggregator' :
                 el = document.getElementsByClassName(html_prefix + 'aggregator')[0];
                 this.loadAggregator();
                 break;
@@ -351,7 +362,36 @@ var Kaleidoscope = {
     },
 
     loadAggregator : function() {
-        console.log('foo');
+
+        var url = '';
+        if (this.config.pageType === "front") {
+            url = this.config.aggregator_networkfront_url;
+        } else if (this.config.pageType === "section") {
+            url = this.config.aggregator_section_base_url + this.config.pageSection;
+        }
+
+        var output = '<div class="' + html_prefix + 'aggregator-inner">';
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: url, // todo: make this a variable
+            onload: function (response) {
+                var data = JSON.parse(response.responseText);
+                if (data.bundles) {
+                    for (var i in data.bundles) {
+                        var bundle = data.bundles[i];
+                        output += '<h4><strong>' + bundle.name + '</strong></h4>';
+                        output += '<ol class="sublinks">';
+                        for (var j in bundle.items) {
+                            var item = bundle.items[j];
+                            output += '<li class="bullet">' + item.webTitle + '</li>';
+                        }
+                        output += '</ol><br /><br />'; // sorry
+                    }
+                    output += '</div>';
+                    document.getElementsByClassName(html_prefix + 'aggregator')[0].innerHTML = output;
+                }
+            }
+        });
     },
 
     destroyFrame : function() {
@@ -384,7 +424,7 @@ var Kaleidoscope = {
                 var headline = document.querySelector('#main-article-info h1');
                 var standfirst = document.querySelector('#stand-first');
                 var articleBody = document.querySelector('#article-body-blocks');
-                var byline = document.querySelector('span.blog-byline-kick');
+                var byline = document.querySelector('span.blog-byline-kick div a'); // this won't always work, needs fixing
 
                 // now make a fake article
                 var placeholder = document.createElement('div');
@@ -402,7 +442,7 @@ var Kaleidoscope = {
                 headlinePlaceholder.innerHTML = headline.innerHTML;
                 standfirstPlaceholder.innerHTML = standfirst.innerHTML;
                 bodyPlaceholder.innerHTML = articleBody.innerHTML;
-                bylinePlaceholder.innerHTML = byline.innerText;
+                bylinePlaceholder.innerHTML = byline.innerHTML;
 
                 // now take out the stuff we don't support
                 var unsupported = placeholder.querySelectorAll('span.embed-media');
@@ -429,8 +469,6 @@ var Kaleidoscope = {
             container.style.top = y + 'px';
         });
     }
-
-
 
 };
 
